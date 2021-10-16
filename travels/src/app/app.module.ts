d8af63ae4710c7669faed0cd71d040711fc2162e
Marco Jost <marco.jost@live.de>
2021-10-16 18:00:08 +0200

    last

--- travels/src/app/app.module.ts
+++ travels/src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContentComponent } from './content/content.component';
-import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeroComponent,
-    FooterComponent,
-    ContentComponent,
-    CardComponent
+    HeroComponent
   ],
   imports: [
     BrowserModule,
